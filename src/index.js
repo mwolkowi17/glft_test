82e295a47bc9a5b7c795a4e0e166c3536802d653
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-07-31 00:53:46 +0200

    next step ready

--- src/index.js
+++ src/index.js
@@ -1,2 +1,70 @@
-import THREE from 'three'
-import GLTFLoader from 'three-gltf-loader';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+import * as OrbitControls from 'three-orbitcontrols';
+
+import { light2, light3, light4 } from './direct_light.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+scene.background = new THREE.Color( 0xE8E8E8 );
+
+scene.add(light2);
+scene.add(light2.target);
+scene.add(light3);
+scene.add(light3.target);
+scene.add(light4);
+scene.add(light4.target);
+
+const gltfLoader2 = new GLTFLoader()
+    gltfLoader2.load('./prasa_gor1.glb', (gltf) => {
+      const root = gltf.scene;
+      scene.add(root);
+    })
+/*
+      // compute the box that contains all the stuff
+      // from root and below
+      const box = new THREE.Box3().setFromObject(root);
+
+      const boxSize = box.getSize(new THREE.Vector3()).length();
+      const boxCenter = box.getCenter(new THREE.Vector3());
+
+      // set the camera to frame the box
+      frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+      // update the Trackball controls to handle the new size
+      controls.maxDistance = boxSize * 10;
+      controls.target.copy(boxCenter);
+      controls.update();
+      
+    });*/
+
+camera.position.z = 8;
+camera.position.y = 1.5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+//camera.position.set( 0, 20, 100 );
+controls.update();
+//controls.autoRotate=true;
+
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    //cube.rotation.x += 0.02;
+    //cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+    
+    controls.update();
+
+};
+
+animate();
